ed12af27105627b40a0fb6711d90af9c4bd9f3c0
Abhilash C S <abhilash.acs14@gmail.com>
2024-03-12 09:13:45 +0530

    clear trash functionality

--- src/pages/TrashPage/TrashPage.vue
+++ src/pages/TrashPage/TrashPage.vue
@@ -4,6 +4,19 @@
       <h2 class="font-semibold text-xl text-black mb-5">
         Trash ({{ $store.state.trashes.length }})
       </h2>
+      <div
+        class="flex items-center gap-3 mb-8"
+        v-if="$store.state.trashes.length"
+      >
+        <span>Clear trash?</span>
+        <button
+          class="text-base text-black underline"
+          type="button"
+          @click="handleClearTrash"
+        >
+          Clear
+        </button>
+      </div>
       <TrashList />
     </Container>
   </section>
@@ -15,6 +28,11 @@ import TrashList from "@components/TrashList/TrashList.vue";
 export default {
   components: {
     TrashList,
+  },
+  methods: {
+    handleClearTrash() {
+      this.$store.state.trashes.splice(0, this.$store.state.trashes.length);
+    },
   },
 };
 </script>
